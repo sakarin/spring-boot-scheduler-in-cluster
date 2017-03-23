0961443a259e51dac66b9e5eec3930990446caca
sakarin <sakarin_rad@truecorp.co.th>
2017-03-23 11:52:46 +0700

    init project

---
+++ src/test/java/th/co/truecorp/omx/SpringBootScheduledInClusterApplicationTests.java
@@ -0,0 +1,16 @@
+package th.co.truecorp.omx;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootScheduledInClusterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
